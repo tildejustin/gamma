24498901292128b8756de99a20bbeb6a50c4dbe0
tildejustin <spacepepper3.14@gmail.com>
2023-07-07 16:55:07 -0400

    change mod structure, because i forgot about inner mixin classes

--- src/main/java/xyz/tildejustin/gamma/EarlyRiser.java
+++ src/main/java/xyz/tildejustin/gamma/EarlyRiser.java
@@ -4,7 +4,7 @@ import com.chocohead.mm.api.ClassTinkerers;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 
-public class GameOptionsOptionMM implements Runnable {
+public class EarlyRiser implements Runnable {
     @Override
     public void run() {
         MappingResolver remapper = FabricLoader.getInstance().getMappingResolver();
--- src/main/java/xyz/tildejustin/gamma/mixin/GameOptionsMixin.java
+++ src/main/java/xyz/tildejustin/gamma/mixin/GameOptionsMixin.java
@@ -1,9 +1,14 @@
 package xyz.tildejustin.gamma.mixin;
 
+import com.chocohead.mm.api.ClassTinkerers;
 import net.minecraft.client.option.GameOptions;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
@@ -21,4 +26,23 @@ public class GameOptionsMixin {
     private void modifyBrightnessSliderNaming(GameOptions.Option option, CallbackInfoReturnable<String> cir, String string, float f, float g) {
         cir.setReturnValue(string + (int) (g * 500.0F) + "%");
     }
+
+    @Mixin(GameOptions.Option.class)
+    public abstract static class GameOptions$OptionMixin {
+        @Mutable
+        @Final
+        @Shadow
+        public static GameOptions.Option BRIGHTNESS;
+
+        @Inject(
+                method = "<clinit>",
+                at = @At(
+                        value = "TAIL"
+                )
+        )
+        private static void changeBrightnessEnum(CallbackInfo ci) {
+            BRIGHTNESS = ClassTinkerers.getEnum(GameOptions.Option.class, "BRIGHTNESS");
+        }
+    }
+
 }
